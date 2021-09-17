bff6186525c569625e24930667d5aa009aadd406
ndrewwang <56122552+ndrewwang@users.noreply.github.com>
2021-09-17 01:52:15 +0100

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-
-data = pd.DataFrame([[pd.Timestamp("2021-01-01"),pd.Timestamp("2021-01-02")], ["abc", "def"]],
-       columns = ["testcol1", "testcol2"])
-st.dataframe(data)
