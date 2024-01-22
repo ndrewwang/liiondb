34d213aa86799dddb8dbb98c0da8b73f60e4e49f
ndrewwang <56122552+ndrewwang@users.noreply.github.com>
2024-01-22 13:39:50 -0500

    Update page_pythonnotebooks.py

--- streamlit_gui/pages/page_pythonnotebooks.py
+++ streamlit_gui/pages/page_pythonnotebooks.py
@@ -1,10 +1,10 @@
 import streamlit as st
 # DASHBOARD PAGE ====================
 def write():
-    st.title(":snake: Using LiionDB with Python Notebooks")
+    st.title(":snake: Access LiionDB with Python Notebooks")
     st.write('---')
 
-    st.write('If you would like to interact with the parameter database with programmatically, check out these Google Colab python notebooks here:')
+    st.write('LiionDB is a PostgreSQL parameter database that can be interacted with programmatically, we provide examples through Google Colab python notebooks here:')
 
     st.markdown('1. [Example LiionDB queries](https://colab.research.google.com/github/ndrewwang/liiondb/blob/main/python%20notebooks/1_Example_Queries.ipynb)')
     st.markdown('2. [Plotting parameter comparisons](https://colab.research.google.com/github/ndrewwang/liiondb/blob/main/python%20notebooks/2_Parameter_Plotter.ipynb)')
